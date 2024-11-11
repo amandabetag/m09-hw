245c1dc72d42448b05a0424773d8cb3a0644cacd
amandabetag <psc4pz@virginia.edu>
2024-11-10 22:44:21 -0500

    Set Up File

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='hw9',
+    version='0.1',
+    description='Module 9 Homework',
+    url='https://github.com/amandabetag/m09-hw',
+    author='Amanda Betag',
+    author_email='psc4pz@virginia.edu',
+    license='MIT',
+    packages=['hw9']
+)
+
